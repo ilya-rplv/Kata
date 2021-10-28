136dacf4189ed0d8972cf6329f239f199e5944bd
Ilya Rapoport Lavie <ilya.rapoport@gong.io>
2021-10-28 14:14:45 +0300

    Original GildedRose from: https://github.com/emilybache/GildedRose-Refactoring-Kata

---
+++ src/test/java/io/gong/kata/gildedrose/GildedRoseTest.java
@@ -0,0 +1,19 @@
+package io.gong.kata.gildedrose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
